b6d8e7fb256e57aaa8f5c1ef9247a18a0d1c37f6
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-05-14 11:28:41 +0530

    Added bonus generation script for Godrej Kheda

---
+++ ATTENDANCE SALARY SHEETS/GODREJ KHEDA/GOD-KHE-BONUS.js
@@ -0,0 +1,32 @@
+function generateBonusForGodrejKheda() {
+    const ui = SpreadsheetApp.getUi();
+  const response = ui.alert('BONUS', 'WOULD YOU LIKE TO RUN BONUS SCRIPT?', ui.ButtonSet.OK_CANCEL);
+  if (response !== ui.Button.OK) return; // Immediately exit if they click Cancel or the 'X'
+  const startTime = Date.now();
+
+  const config1 = {
+    templateID: '1WYQNmyx_kYGBggFAWvCkJZmn9jLyremyLAPVo5c_PXQ',
+    targetFolderID: '1H_jz4Ajxd4CFgCSfU0NjLJDvVbGuRktz',
+    attendanceSlipID: '1dNQZq398LFy76kzpWN4733zdtns9LjJoBgtq6Mt-FMU',
+
+    siteFolderName: 'Godrej Kheda',
+    dates: 0, // Date control: 0 = previous month, 1 = current month
+
+    formConfig: {
+      columns: [
+        'Sl. No.',
+        'Employee Code',
+        'Name',
+        {
+          type: 'empty',
+          value: 'Godrej Kheda'
+        },
+        'Remarks'
+      ]
+    }
+  };
+  Documents.generateBonusDocument(config1);
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+}
