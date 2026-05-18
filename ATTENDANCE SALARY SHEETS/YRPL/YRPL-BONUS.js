e22f40470dacdff078df3a223b17a1d0a06edd5b
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-05-18 16:49:16 +0530

    Added script to update/generate Bonus document for YRPL

---
+++ ATTENDANCE SALARY SHEETS/YRPL/YRPL-BONUS.js
@@ -0,0 +1,32 @@
+function generateBonusForYRPL() {
+  const ui = SpreadsheetApp.getUi();
+  const response = ui.alert('BONUS', 'WOULD YOU LIKE TO RUN BONUS SCRIPT?', ui.ButtonSet.OK_CANCEL);
+  if (response !== ui.Button.OK) return; // Immediately exit if they click Cancel or the 'X'
+  const startTime = Date.now();
+
+  const config1 = {
+    templateID: '1WYQNmyx_kYGBggFAWvCkJZmn9jLyremyLAPVo5c_PXQ',
+    targetFolderID: '1H_jz4Ajxd4CFgCSfU0NjLJDvVbGuRktz',
+    attendanceSlipID: '1idVteb5gGED-o3wl7EINSsSrcXGS9RN9vKKYH1NcwCY',
+
+    siteFolderName: 'YRPL',
+    dates: 0, // Date control: 0 = previous month, 1 = current month
+
+    formConfig: {
+      columns: [
+        'Sl. No.',
+        'Employee Code',
+        'Name',
+        {
+          type: 'empty',
+          value: 'YRPL'
+        },
+        'Remarks'
+      ]
+    }
+  };
+  Documents.generateBonusDocument(config1);
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+}
