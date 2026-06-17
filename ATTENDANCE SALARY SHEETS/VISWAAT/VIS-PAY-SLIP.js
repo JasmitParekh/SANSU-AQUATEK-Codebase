5b2fe7f5020378e7ecb0d7dd1950045b07abd90a
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-06-17 16:47:10 +0530

    Added script to generate payment slips for Viswaat

---
+++ ATTENDANCE SALARY SHEETS/VISWAAT/VIS-PAY-SLIP.js
@@ -0,0 +1,44 @@
+function generatePaymentRecieptsForViswaat() {
+  const ui       = SpreadsheetApp.getUi();
+  const response = ui.alert(
+    'PAYMENT SLIPS',
+    'WOULD YOU LIKE TO GENERATE PAYMENT SLIP FOR VISWAAT?',
+    ui.ButtonSet.OK_CANCEL
+  );
+  if (response !== ui.Button.OK) return;
+  const startTime = Date.now();
+
+  const config = {
+    // ── Source ──────────────────────────────────────────────────────────────
+    sourceSheetId:  '1NCLi8kCk7fgYpYopl3th63ckv752sdePassD4xaTccg', // Viswaat Attendance Salary Sheet
+    sourceTabName:  null,                // null = first sheet
+
+    // ── Template ────────────────────────────────────────────────────────────
+    templateId:     '1Kq1eUuyoP-o0x1iDId_s3fxj73I9cY5qfL2El6ifxQE',          // Google Docs template
+
+    // ── Output ──────────────────────────────────────────────────────────────
+    outputMode:     'COMBINED',         // 'ONE_PER_ROW' | 'COMBINED',
+    outputFileName: 'Payment Slips', // <<tags>> in filename
+
+    siteFolderName: 'Viswaat Chemicals',  // uses standard month-folder hierarchy
+    dates:          0,                    // 0 = previous month
+
+    // ── Filtering ───────────────────────────────────────────────────────────
+    rowFilter:      null,                 // null = all rows
+
+    // ── Tag Resolution ───────────────────────────────────────────────────
+     tagMap: {
+      '<<Month>>': 'Date on which Overtime Worked'
+    }
+
+  };
+
+  Documents.generateMailMerge(config);
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert(
+    'EXECUTION COMPLETE',
+    `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`,
+    ui.ButtonSet.OK
+  );
+}
