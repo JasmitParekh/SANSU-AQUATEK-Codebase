d2f60ab5417b4b0a1561c0617d1ce85cecd1b936
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-06-23 12:05:28 +0530

    Enhance DCM form generation with departments and categories
    Refactor form generation to include department and category filters.

--- ATTENDANCE SALARY SHEETS/DCM/DCM-FORM-18.js
+++ ATTENDANCE SALARY SHEETS/DCM/DCM-FORM-18.js
@@ -1,10 +1,21 @@
-function generateForm18ForDCM() {
+function generateForm18WithDepartmentsForDCM(){
   const ui = SpreadsheetApp.getUi();
   const response = ui.alert('FORM-18', 'WOULD YOU LIKE TO UPDATE FORM-18?', ui.ButtonSet.OK_CANCEL);
   if (response !== ui.Button.OK) return;
   const startTime = Date.now();
 
-  const config = {
+  const departments = SITE_CONFIG.departments_form18 || [null]; // This should match with the DEPARTMENT NAME column in EMPLOYEE DETAILS SHEET.
+  
+  departments.forEach(department => {
+    generateForm18ForDCM(department)
+  })
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+}
+
+function generateForm18ForDCM(departmentName) {
+  const baseConfig = {
     // ── Source data ───────────────────────────────────────────────────────
     attendanceSlipId: SITE_CONFIG.attendanceSalarySheetID,
     leaveWageSheetId: SITE_CONFIG.leaveWageSheetID,
@@ -17,17 +28,23 @@ function generateForm18ForDCM() {
     dates: SITE_CONFIG.dates,                                                       
 
     // ── Optional filters ──────────────────────────────────────────────────
-    plantSheetName: SITE_CONFIG.departmentName,
+    plantSheetName: departmentName,
     categoryFilter: SITE_CONFIG.categoryFilter,                                     
 
     // ── Form-18 generation ────────────────────────────────────────────────
-    generateForm18:   true,
+    generateForm18:   SITE_CONFIG.generateForm18,
     form18FolderId: SITE_CONFIG.form18FolderID,
     form18TemplateId: SITE_CONFIG.form18TemplateID
   };
 
-  Documents.generateForm18(config);
+  const categories = SITE_CONFIG.categories_form18 || [null];      // Provide the categories for which the forms are to be generated.
+  categories.forEach(category => {                                 // This should match with the category column in EMPLOYEE DETAILS SHEET.
+    generateForm18WithCategoryFilterForDCM(baseConfig, category);
+  })
+}
 
-  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
-  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+function generateForm18WithCategoryFilterForDCM(baseConfig, category){
+    const config = JSON.parse(JSON.stringify(baseConfig));
+    config.categoryFilter = category;
+    Documents.generateForm18(config);                          // Call the Documents library
 }
