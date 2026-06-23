627ad415c87a117253869d869eb06cad0953cbe3
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-06-23 12:41:37 +0530

    Refactor form generation to include departments and categories

--- ATTENDANCE SALARY SHEETS/GODREJ KHEDA/GOD-KHE-FORM-16,17,20_TO_22.js
+++ ATTENDANCE SALARY SHEETS/GODREJ KHEDA/GOD-KHE-FORM-16,17,20_TO_22.js
@@ -1,22 +1,33 @@
-function generateFormsFromAttendanceSlip() {
+function generateWithDepartmentsGodrejKheda(){
   const ui = SpreadsheetApp.getUi();
-  const response = ui.alert('FORM-16,17,20,21,22', 'WOULD YOU LIKE TO GENERATE FORM-16,17,20 TO 22?', ui.ButtonSet.OK_CANCEL);
-  if (response !== ui.Button.OK) return; // Immediately exit if they click Cancel or the 'X'
+  const response = ui.alert('FORM-16,17,20 TO 22', 'WOULD YOU LIKE TO GENERATE FORM-16,17,20 TO 22?', ui.ButtonSet.OK_CANCEL);
+  if (response !== ui.Button.OK) return;
   const startTime = Date.now();
 
-  const config = {
-    formatSpreadsheetId: '1n_g0HBKhTWvnin7VTm6UvHxPqg3qpDKAfshLQtaQs_0',
-    attendanceSlipId: '1dNQZq398LFy76kzpWN4733zdtns9LjJoBgtq6Mt-FMU',
-    siteFolderName: 'Godrej Kheda',
-    dates: 0,
+  const departments = SITE_CONFIG.departments_salary || [null];      // This should match with the DEPARTMENT NAME column in EMPLOYEE DETAILS SHEET.
+
+  departments.forEach(department => {
+      generateFormsFromAttendanceSlip(department?.name ?? department);
+  })
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+}
+
+function generateFormsFromAttendanceSlip(departmentName) {
+  const baseConfig = {
+    formatSpreadsheetId: SITE_CONFIG.formatTemplateID,
+    attendanceSlipId: SITE_CONFIG.attendanceSalarySheetID,
+    siteFolderName: SITE_CONFIG.siteFolderName,
+    dates: SITE_CONFIG.dates,
 
     // Define which forms to generate
     forms: [
-      { sheetName: 'Form-16_Format', fileName: '5. Form-16: Attendance Register', configKey: 'form16' },
-      { sheetName: 'Form-17_Format', fileName: '6. Form-17: Wages Register', configKey: 'form17' },
-      { sheetName: 'Form-20_Format', fileName: '7. Form-20: Register of Damage and Loss', configKey: 'form20' },
-      { sheetName: 'Form-21_Format', fileName: '8. Form-21: Register of Fines', configKey: 'form21' },
-      { sheetName: 'Form-22_Format', fileName: '9. Form-22: Advance Register', configKey: 'form22' }
+      { sheetName: 'Form-16_Format', fileName: `5. Form-16: Attendance Register${departmentName ? ` (${departmentName})` : ''}`, configKey: 'form16' },
+      { sheetName: 'Form-17_Format', fileName: `6. Form-17: Wages Register${departmentName ? ` (${departmentName})` : ''}`, configKey: 'form17' },
+      { sheetName: 'Form-20_Format', fileName: `7. Form-20: Register of Damage and Loss${departmentName ? ` (${departmentName})` : ''}`, configKey: 'form20' },
+      { sheetName: 'Form-21_Format', fileName: `8. Form-21: Register of Fines${departmentName ? ` (${departmentName})` : ''}`, configKey: 'form21' },
+      { sheetName: 'Form-22_Format', fileName: `9. Form-22: Advance Register${departmentName ? ` (${departmentName})` : ''}`, configKey: 'form22' }
     ],
     
     formConfigs: {
@@ -27,11 +38,11 @@ function generateFormsFromAttendanceSlip() {
           'Name',
           { type: 'empty' },
           // Date columns (26-31, 1-25)
-          ...["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27",  "28","29","30","31"],
+          ...["26","27", "28","29","30","31","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25"],
           { 
             type: 'formula',
             header: 'Summary No. of Days', 
-            formula: '=COUNTIF({1}:{31}, "P")+COUNTIF({1}:{31}, "PH")+COUNTIF({1}:{31}, "P/2")*0.5+COUNTIF({1}:{31}, "L")' 
+            formula: '=COUNTIF({26}:{25}, "P")+COUNTIF({26}:{25}, "PH")+COUNTIF({26}:{25}, "P/2")*0.5+COUNTIF({26}:{25}, "L")' 
           },
           { type: 'empty' },
           'Remarks'
@@ -101,11 +112,12 @@ function generateFormsFromAttendanceSlip() {
             header: 'Others (PT)',
             formula: '=IF({Total}>12000, 200, 0)'
           },
+          'FINE',
           'Advance',
           {
             type: 'formula',
             header: 'Net Payment',
-            formula: '={Total}-{PF}-{Others (PT)}-{Advance}'
+            formula: '={Total}-{PF}-{Others (PT)}-{FINE}-{Advance}'
           },
           { type: 'empty' },
           'Initial of Contractor or his Representative'
@@ -119,9 +131,15 @@ function generateFormsFromAttendanceSlip() {
     }
   };
 
-  // Call the library function
-  Documents.generateAttForms(config);
+  const categories = SITE_CONFIG.categories_salary || [null];              // Provide the categories for which the forms are to be generated.
+  categories.forEach(category => {                                         // This should match with the category column in Attendance_Salary_Sheet.
+    generateWithCategoryFilterGodrejKheda(baseConfig, category);
+  })
+}
 
-  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
-  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+function generateWithCategoryFilterGodrejKheda(baseConfig, category){
+    const config = JSON.parse(JSON.stringify(baseConfig));
+    config.subFolder = category;
+    config.categoryFilter = category;
+    Documents.generateAttForms(config);                                             // Call the Documents library
 }
