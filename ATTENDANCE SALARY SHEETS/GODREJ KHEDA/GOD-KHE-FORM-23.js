2302e5e50173e38838a38bd50ce2bdaf22e876d5
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-06-23 12:42:15 +0530

    Refactor generateForm23 to include departments and categories

--- ATTENDANCE SALARY SHEETS/GODREJ KHEDA/GOD-KHE-FORM-23.js
+++ ATTENDANCE SALARY SHEETS/GODREJ KHEDA/GOD-KHE-FORM-23.js
@@ -1,18 +1,29 @@
-function generateForm23() {
-    const ui = SpreadsheetApp.getUi();
+ function generateForm23WithDepartmentsGodrejKheda(){
+  const ui = SpreadsheetApp.getUi();
   const response = ui.alert('FORM-23', 'WOULD YOU LIKE TO GENERATE FORM-23?', ui.ButtonSet.OK_CANCEL);
-  if (response !== ui.Button.OK) return; // Immediately exit if they click Cancel or the 'X'
+  if (response !== ui.Button.OK) return;
   const startTime = Date.now();
 
-  const config = {
-    formatSpreadsheetId: '1n_g0HBKhTWvnin7VTm6UvHxPqg3qpDKAfshLQtaQs_0',
-    attendanceSlipId: '1dNQZq398LFy76kzpWN4733zdtns9LjJoBgtq6Mt-FMU',
-    siteFolderName: 'Godrej Kheda',
-    dates: 0,
+  const departments = SITE_CONFIG.departments_salary || [null];       // This should match with the DEPARTMENT NAME column in EMPLOYEE DETAILS SHEET.
+
+  departments.forEach(department => {
+      generateForm23(department?.name ?? department);
+  })
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+}
+
+function generateForm23(departmentName) {
+  const baseConfig = {
+    formatSpreadsheetId: SITE_CONFIG.formatTemplateID,
+    attendanceSlipId: SITE_CONFIG.attendanceSalarySheetID,
+    siteFolderName: SITE_CONFIG.siteFolderName,
+    dates: SITE_CONFIG.dates,
 
     // Define which forms to generate
     forms: [
-      { sheetName: 'Form-23_Format', fileName: '10. Form-23: Register of Overtime', configKey: 'form23' }
+      { sheetName: 'Form-23_Format', fileName: `10. Form-23: Register of Overtime${departmentName ? ` (${departmentName})` : ''}`, configKey: 'form23' }
     ],
     
     formConfigs: {
@@ -49,9 +60,15 @@ function generateForm23() {
     }
   };
 
-  // Call the library function
-  Documents.generateAttForms(config);
+  const categories = SITE_CONFIG.categories_salary || [null];                     // Provide the categories for which the forms are to be generated.
+  categories.forEach(category => {                                                // This should match with the category column in Attendance_Salary_Sheet.
+    generateForm23WithCategoryFilterGodrejKheda(baseConfig, category);
+  })
+}
 
-  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
-  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+function generateForm23WithCategoryFilterGodrejKheda(baseConfig, category){
+    const config = JSON.parse(JSON.stringify(baseConfig));
+    config.subFolder = category;
+    config.categoryFilter = category;
+    Documents.generateAttForms(config);                                             // Call the Documents library
 }
