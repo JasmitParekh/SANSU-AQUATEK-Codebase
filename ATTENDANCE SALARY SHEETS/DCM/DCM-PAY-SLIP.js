6b9b516083b99185ec0a09795aaf75122aa19a6b
Jasmit Pareshbhai Parekh <144789404+JasmitParekh@users.noreply.github.com>
2026-06-23 11:08:50 +0530

    Refactor payment slip generation to include departments

--- ATTENDANCE SALARY SHEETS/DCM/DCM-PAY-SLIP.js
+++ ATTENDANCE SALARY SHEETS/DCM/DCM-PAY-SLIP.js
@@ -1,24 +1,31 @@
-function generatePaymentRecieptsForDCM() {
-  const ui       = SpreadsheetApp.getUi();
-  const response = ui.alert(
-    'PAYMENT SLIPS',
-    'WOULD YOU LIKE TO GENERATE PAYMENT SLIP FOR DCM?',
-    ui.ButtonSet.OK_CANCEL
-  );
+function generatePaySlipWithDepartmentsForDCM(){
+  const ui = SpreadsheetApp.getUi();
+  const response = ui.alert('PAYMENT SLIPS', 'WOULD YOU LIKE TO GENERATE PAYMENT SLIPS FOR DCM?', ui.ButtonSet.OK_CANCEL);
   if (response !== ui.Button.OK) return;
   const startTime = Date.now();
 
+  const departments = SITE_CONFIG.departments_payment || [null]; // This should match with the DEPARTMENT NAME column in EMPLOYEE DETAILS SHEET.
+  
+  departments.forEach(department => {
+    generatePaymentRecieptsForDCM(department)
+  })
+
+  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
+  ui.alert('EXECUTION COMPLETE', `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`, ui.ButtonSet.OK);
+}
+
+function generatePaymentRecieptsForDCM(departmentName) {
   const config = {
     // ── Source ──────────────────────────────────────────────────────────────
     sourceSheetId: SITE_CONFIG.attendanceSalarySheetID,
-    sourceTabName: SITE_CONFIG.departmentName,
+    sourceTabName: departmentName,
 
     // ── Template ────────────────────────────────────────────────────────────
     templateId: SITE_CONFIG.paySlipTemplateID,
 
     // ── Output ──────────────────────────────────────────────────────────────
     outputMode: 'COMBINED',                                                   // 'ONE_PER_ROW' | 'COMBINED',
-    outputFileName: 'Payment Slips',                                          // <<tags>> in filename
+    outputFileName: `Payment Slips${departmentName ? ` (${departmentName})` : ''}`,  // <<tags>> in filename
 
     siteFolderName: SITE_CONFIG.siteFolderName,
     dates: SITE_CONFIG.dates,
@@ -35,11 +42,4 @@ function generatePaymentRecieptsForDCM() {
   };
 
   Documents.generateMailMerge(config);
-
-  const timeTaken = ((Date.now() - startTime) / 1000).toFixed(2);
-  ui.alert(
-    'EXECUTION COMPLETE',
-    `CODE EXECUTION IS COMPLETED.\nTIME TAKEN: ${timeTaken} SECONDS.`,
-    ui.ButtonSet.OK
-  );
 }
